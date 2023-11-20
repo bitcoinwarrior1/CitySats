9d19b80d90f70b406eb661c2436ba7a9b7f740d9
James-Sangalli <j.l.sangalli@gmail.com>
2023-11-20 13:42:32 +1100

    wip add markers

--- src/app/components/Map.tsx
+++ src/app/components/Map.tsx
@@ -1,23 +1,36 @@
 "use client"
 import GoogleMap from "google-maps-react-markers";
+import {coordinates, Marker} from "@/app/components/Marker";
 
 const mapContainerStyle = {
-    width: '65vw',
-    height: '35vw',
-    'margin-top': '5%',
+    width: '90vw',
+    height: '85vw',
+    'margin-top': '1%',
     'margin-bottom': '1%'
 };
 
 const center = {
-    lat: 33.8955044, // Set your default latitude
-    lng: -151, // Set your default longitude
+    lat: -33.865143, // Set your default latitude
+    lng: 151.209900, // Set your default longitude
 };
 
 const Map = ({ }) => {
     return (
         <div id={'map'}>
             <GoogleMap
-                apiKey={"AIzaSyAQWSnntCOZdRP6hAKv2wR9nLGwOv69BZ0"} defaultCenter={center} defaultZoom={5} style={mapContainerStyle}>
+                apiKey={"AIzaSyAQWSnntCOZdRP6hAKv2wR9nLGwOv69BZ0"} defaultCenter={center} defaultZoom={12} style={mapContainerStyle}>
+                {coordinates.map((data, index) => (
+                    <Marker
+                        key={index}
+                        lat={data.lat}
+                        lng={data.lng}
+                        markerId={data.markerId}
+                        onClick={data.onClick}
+                        buyer={data.buyer}
+                        seller={data.seller}
+                        image={data.image}
+                    ></Marker>
+                ))}
             </GoogleMap>
         </div>
     );
